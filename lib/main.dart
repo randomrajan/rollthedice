d46d2532f9ce0074a27f230e1c238f9579a8e716
theflaky <rajan2499@gmail.com>
2020-09-07 14:29:01 +0530

    init commit

--- lib/main.dart
+++ lib/main.dart
@@ -16,15 +16,50 @@ void main() {
   );
 }
 
-class DicePage extends StatelessWidget {
+class DicePage extends StatefulWidget {
+  @override
+  _DicePageState createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  Random rndm = new Random();
+  int leftDiceNumber = 5;
+  int rightDiceNumber = 4;
   @override
   Widget build(BuildContext context) {
-    return Row(
-      children: [
-        Image(
-          image: AssetImage('images/dice1.png'),
-        ),
-      ],
+    //leftDiceNumber = 2;
+
+    return Center(
+      child: Row(
+        children: [
+          Expanded(
+            //flex: 2,
+            child: FlatButton(
+              onPressed: (){                                                  //void callback
+                setState(() {
+                  leftDiceNumber = rndm.nextInt(6) + 1;
+                });
+              },
+              child: Image.asset('images/dice$leftDiceNumber.png'),
+            ),
+          ),
+          Expanded(
+            //flex: 1,
+            child: FlatButton(
+              onPressed: (){                                                  //void callback
+                setState(() {
+                  rightDiceNumber = rndm.nextInt(6) + 1;
+                });
+              },
+              child: Image(
+                image: AssetImage('images/dice$rightDiceNumber.png'),
+              ),
+            ),
+          )
+        ],
+      ),
     );
   }
 }
+
+
